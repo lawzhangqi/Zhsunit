3c713aa2aa2c9c535d30cdb30ba7af6676b374b4
章琦 <Zhangqi@ZhangqimatoiMac.local>
2012-03-07 20:40:43 +0800

    Initial Commit

---
+++ ZhsunIt/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  ZhsunIt
+//
+//  Created by 琦 章 on 12-3-7.
+//  Copyright (c) 2012年 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ ZhsunIt/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  ZhsunIt
+//
+//  Created by 琦 章 on 12-3-7.
+//  Copyright (c) 2012年 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
