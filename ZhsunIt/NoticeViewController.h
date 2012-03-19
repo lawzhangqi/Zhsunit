5d9e0950ece827ad6161d7e9f34f0ac5c036902b
zhangqi <zhangqi@zhsunit.com>
2012-03-19 23:21:49 +0800

    修改部分界面，添加视图代码文件

---
+++ ZhsunIt/NoticeViewController.h
@@ -0,0 +1,18 @@
+//
+//  NoticeViewController.h
+//  ZhsunIt
+//
+//  Created by 琦 章 on 12-3-19.
+//  Copyright (c) 2012年 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "TKAlertCenter.h"
+
+@interface NoticeViewController : UIViewController{
+    
+}
+@property (retain, nonatomic) IBOutlet UIButton *NoticeBt;
+- (IBAction)NoticeBt:(id)sender;
+
+@end
---
+++ ZhsunIt/NoticeViewController.m
@@ -0,0 +1,64 @@
+//
+//  NoticeViewController.m
+//  ZhsunIt
+//
+//  Created by 琦 章 on 12-3-19.
+//  Copyright (c) 2012年 __MyCompanyName__. All rights reserved.
+//
+
+#import "NoticeViewController.h"
+
+@interface NoticeViewController ()
+
+@end
+
+@implementation NoticeViewController
+@synthesize NoticeBt;
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    
+    
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)viewDidUnload
+{
+    [self setNoticeBt:nil];
+    [super viewDidUnload];
+    // Release any retained subviews of the main view.
+}
+
+- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
+{
+    return (interfaceOrientation == UIInterfaceOrientationPortrait);
+}
+
+- (void)dealloc {
+    [NoticeBt release];
+    [super dealloc];
+}
+
+
+- (IBAction)NoticeBt:(id)sender {
+//[[TKAlertCenter defaultCenter] postAlertWithMessage:@"您的网络好像有问题哦！"];
+    
+ 
+
+}
+
+
+
+
+
+@end
